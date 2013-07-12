40a9e9bb76aee325ca9f7146db911d260a872893
Christopher Nguyen <christoph.nguyen@gmail.com>
2013-07-11 17:04:34 -0700

    More heroku setup

--- iluvpy/settings.py
+++ iluvpy/settings.py
@@ -1,6 +1,4 @@
 # Django settings for iluvpy project.
-import os
-PROJECT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -165,3 +163,25 @@ LOGGING = {
         },
     }
 }
+
+"""
+For heroku usage
+"""
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+import os
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
